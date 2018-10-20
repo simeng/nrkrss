21fe5510436f4ae5c4761307cfec3411c54aea37
Simen Graaten <simeng@gmail.com>
2018-10-20 22:22:44 +0200

    Python3ize and fetch segments

--- test/rss.py
+++ test/rss.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
 import json, re
 from datetime import datetime
 import pytz
-
+import sys
+import m3u8
 
 def fetchSeasons(name):
     url = 'https://radio.nrk.no/psapi/series/%s' % ( name )
@@ -11,19 +13,19 @@ def fetchSeasons(name):
         
         playlist = json.loads(p)
 
-        print "%s, %s" % (playlist['title'], playlist['description'])
+        print("{}, {}".format(playlist['title'], playlist['description']))
 
         for s in playlist['seasons']:
             if s['hasOnDemandRightsEpisodes']:
-                print "%s: %s (%s)" % (s['id'], s['name'], url)
+                print("%s: %s (%s)".format(s['id'], s['name'], url))
 
                 episodes = fetchEpisodes(s['id'])
-                print episodes
+                print(episodes)
                 break
 
 
 def fetchEpisodes(id):
-    url = 'https://radio.nrk.no/psapi/series/dagsnytt/seasons/%s/Episodes' % ( id )
+    url = 'https://radio.nrk.no/psapi/series/dagsnytt/seasons/{}/Episodes'.format(id)
     url = 'Episodes' # temp
     with open(url) as f:
         p = f.read()
@@ -31,10 +33,10 @@ def fetchEpisodes(id):
         episodes = json.loads(p)
 
         for e in episodes:
-            print "seasonId: %s, seriesTitle: %s, episodeId: %s, episode %d, title: %s, date: %s" % ( e['seasonId'], e['seriesTitle'],  e['id'], e['episodeNumber'], e['episodeTitle'], e['episodeNumberOrDate'] )
+            print("seasonId: {}, seriesTitle: {}, episodeId: {}, episode {}, title: {}, date: {}".format(e['seasonId'], e['seriesTitle'],  e['id'], e['episodeNumber'], e['episodeTitle'], e['episodeNumberOrDate']))
             try:
                 publicationDate = e['firstTimeTransmitted']['actualTransmissionDate']
-            except TypeError, e:
+            except TypeError as e:
                 publicationDate = None
 
             if publicationDate: 
@@ -44,6 +46,21 @@ def fetchEpisodes(id):
                     timestamp = float(dateString[0]) / 1000
                     timezone = dateString[1]
                     publicationDate = datetime.fromtimestamp(timestamp)
-                    print "publicationDate: %s" % ( publicationDate )
+                    print("publicationDate: {}".format(publicationDate))
+                    try:
+                        for asset in e['mediaAssetsOnDemand']:
+                            print("media assets: {}".format(asset['hlsUrl']))
+                            master = m3u8.load("master.m3u8")
+                            for playlist in master.playlists:
+                                print(playlist.uri)
+                                streamdata = m3u8.load("index_0_a.m3u8")
+                                for segment in streamdata.segments:
+                                    print(segment.uri)
+                            sys.exit()
+
+                    except TypeError as e:
+                        print("No media assets: {}".format(e))
+
+
 
 index = fetchSeasons('dagsnytt')
